f331d8f6b3d66ec9521f54327e3b779fba87d697
Billy Huang <10646003@ntub.edu.tw>
2024-01-05 15:00:23 +0800

    feat(index): add change language buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import logo from './logo.svg';
+import { useTranslation } from 'react-i18next';
 import './App.css';
+import i18n from './i18next';
+import logo from './logo.svg';
 
 function App() {
+  const { t } = useTranslation();
+
+  function changeLanguage(lang) {
+    i18n.changeLanguage(lang, (err, t) => {
+      if (err) return console.log('something went wring loading', err);
+      t('greet');
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => changeLanguage('en')}>en</button>
+        <button onClick={() => changeLanguage('zh')}>zh</button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
+          {t('greet')}
         </a>
       </header>
     </div>
--- src/i18next.js
+++ src/i18next.js
@@ -1,18 +1,20 @@
-import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
-import { en, zh } from "./locales";
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+import { en, zh } from './locales';
 i18n
-.use(Backend)
-.use(LanguageDetector)
-.use(initReactI18next)
-.init({
-   fallbackLng: "en",
-   debug: true,
-   resources: { en, zh },
-   interpolation: {
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    resources: { en, zh },
+    interpolation: {
       escapeValue: false,
-      default
-   }
-});export default i18n;
+      //   default,
+    },
+  });
+
+export default i18n;
